2413c75ec1d89bae6bcfdd965f6a496c1e02660b
Suman Ghosh <suman.ghosh@coppernine.com>
2016-02-17 15:18:00 +0530

    Trying handlebars external templates

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -44,20 +44,34 @@ var app = {
     // Update DOM on a Received Event
     receivedEvent: function() {
 
-        console.log("................Initializing deviceInfo variable............");
-        var deviceInfo = cordova.require("cordova/plugin/DeviceInformation");
-        console.log("................deviceInfo variable Initialized : ............" + deviceInfo);
-        deviceInfo.get(function(result) {
-            console.log("In deviceInfo success block");
-            alert("result = " + result);
-        }, function() {
-            console.log("In deviceInfo error block");
-            alert("error");
+
+        app.set_app_view("main_menu");
+
+        $('#new_task_button').click(function(){
+          alert("new task button clicked");
+          app.set_app_view("add_task");
         });
+
+        //console.log("................Initializing deviceInfo variable............");
+        var deviceInfo = cordova.require("cordova/plugin/DeviceInformation");
+        //console.log("................deviceInfo variable Initialized : ............" + deviceInfo);
+        deviceInfo.get(function(result) {
+            //console.log("In deviceInfo success block");
+            //alert("result = " + result);
+        }, function() {
+            //console.log("In deviceInfo error block");
+            //alert("error");
+        });
+
+
+
+
+
+
 
         // ****** Code related to sqlite DB ************
         db = window.sqlitePlugin.openDatabase({name: "doit.db", key: "000000"}, function(db){ console.log("DB variable assigned"); }, function(err){ console.log('Open database ERROR: ' + JSON.stringify(err)); });
-        alert("db variable is" + JSON.stringify(db));
+        //alert("db variable is" + JSON.stringify(db));
 
         // create table if it doesn't exists
         app.create_table();
@@ -93,9 +107,9 @@ var app = {
           if($('#data_input_to_encrypt').val() === "")
             alert("You must enter some data!");
           else{
-            alert("Starting encryption !");
+            //alert("Starting encryption !");
             encryptedData = app.encrypt($('#data_input_to_encrypt').val());
-            alert("Encrypted data is " + encryptedData);
+            //alert("Encrypted data is " + encryptedData);
             $('#encrypted_data').val(encryptedData);
             encryptedData = null;
           }
@@ -106,9 +120,9 @@ var app = {
           if($('#encrypted_data').val() === "")
             alert("You must enter some data!");
           else{
-            alert("Starting decryption !");
+            //alert("Starting decryption !");
             var decryptedData = app.decrypt($('#encrypted_data').val());
-            alert("Decrypted data is " + decryptedData);
+            //alert("Decrypted data is " + decryptedData);
             $('#decrypted_data').val(decryptedData);
             decryptedData = null ;
           }
@@ -150,7 +164,7 @@ var app = {
           alert(" Tasks table created successfully");
         },
         function(error) {
-        alert("Error occurred while creating the table.");
+          alert("Error occurred while creating the table.");
         });
       });
     },
@@ -163,12 +177,6 @@ var app = {
           //alert('All tasks retrieved from database');
           //alert("Retrieved data is " + JSON.stringify(result.rows.item(0).name));
           all_tasks = result
-          for (var i=0; i < result.rows.length; i++){
-              row = result.rows.item(i);
-              name = result.rows.item(i).name
-              alert("row is " + JSON.stringify(row));
-              alert("Name is: " + name);
-          }
           for (i = 0; i < result.rows.length; i++){
             console.log("Stored data");
             console.log(JSON.stringify(result.rows.item(i)));
@@ -210,57 +218,23 @@ var app = {
       }
       else
         return(null);
+    },
+    set_app_view: function(view){
+      switch(view) {
+        case "main_menu":
+          $('#app-view-holder').html(Handlebars.compile($("#main-menu-template").html()));
+          $('#app-view-holder').trigger("create").animate();
+          break;
+        case "add_task":
+          $('#app-view-holder').html(Handlebars.compile($("#add-task-form-template").html()));
+          $('#app-view-holder').trigger("create").animate();
+          break;
+        case "task_list":
+          break;
+        default:
+          $('#app-view-holder').html(Handlebars.compile($("#main-menu-template").html()));
+          $('#app-view-holder').trigger("create");
+      }
     }
 
 };
-
-
-//db related functions
-
-// create table
-// function create_table(){
-//   db.transaction(function(transaction) {
-//     transaction.executeSql('CREATE TABLE IF NOT EXISTS tasks (id integer primary key, name text, status text, complete_by text)', [],
-//     function(tx, result) {
-//       alert(" Tasks table created successfully");
-//     },
-//     function(error) {
-//     alert("Error occurred while creating the table.");
-//     });
-//   });
-// }
-
-
-
-// function to get data about tasks from database
-// function get_tasks_list(){
-//   db.transaction(function(transaction) {
-//     var executeQuery = "SELECT * FROM tasks";
-//     transaction.executeSql(executeQuery, [],
-//     function(tx, result) {
-//       alert('All tasks retrieved from database');
-//       alert("Retrieved data is " + JSON.stringify(result));
-//       //all_tasks = result
-//     },
-//     function(error){
-//       alert('Error retrieving tasks');
-//     });
-//   });
-// }
-
-// Add new task to tasks table
-// function add_task(new_task){
-//
-//   // console.log("................In function add task...................");
-//   // tx.executeSql("INSERT INTO tasks (name, status, complete_by) VALUES (?, ?, ?)", [ new_task.name , new_task.status, new_task.complete_by ]);
-//   db.transaction(function(transaction) {
-//   var executeQuery = "INSERT INTO tasks (name, status, complete_by) VALUES (?, ?, ?)";
-//   transaction.executeSql(executeQuery, [ new_task.name , new_task.status, new_task.complete_by ],
-//     function(tx, result) {
-//       alert('Added task');
-//     },
-//     function(error){
-//       alert('Error adding task');
-//     });
-//   });
-// }
